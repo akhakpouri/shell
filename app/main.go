42412e04563d0f3e6df808f0df8ea524a4374f66
Ali Khakpouri <ali.khakpouri@transunion.com>
2025-10-02 14:08:56 -0400

    fixed the stage

--- app/main.go
+++ app/main.go
@@ -10,9 +10,6 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
+	fmt.Fprint(os.Stdout, "$ ")
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
