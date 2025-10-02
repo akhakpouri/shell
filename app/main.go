a18e93e38e9bc2d050c8984f4b2e47ccfc54f47e
Ali Khakpouri <ali.khakpouri@transunion.com>
2025-10-02 14:40:39 -0400

    fixed the for loop

--- app/main.go
+++ app/main.go
@@ -10,19 +10,19 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	command := bufio.NewScanner(os.Stdin)
+	run()
+}
 
-	// if err != nil {
-	// 	fmt.Fprintln(os.Stderr, "Error reading input: ", err)
-	// 	os.Exit(1)
-	// }
+func run() {
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	for command.Scan() {
-		line := command.Text()
-		if line == "exit" {
-			break
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+			os.Exit(1)
 		}
-		fmt.Println(command.Text() + ": command not found")
+		fmt.Println(command[:len(command)-1] + ": command not found")
+
 	}
 }
